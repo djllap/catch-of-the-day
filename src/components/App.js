10eac789bd816a96c254c7aaa27d9e1834cd4259
djllap <kentpercussion@gmail.com>
2016-10-26 12:15:18 -0600

    Added Sold out styles and made 'add to order' button update state

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,9 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+
+    // get initial state
     this.state = {
       fishes: {},
       order: {}
@@ -35,13 +38,22 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    // copy of state
+    const order = {...this.state.order};
+    // update/add new fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update state
+    this.setState({ order }) 
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
           <ul className="list-of-fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)}
           </ul>
         </div>
         <Order />
