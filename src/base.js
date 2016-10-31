66ab7a67ca62d3dd2fe5f00e4719d75c1dcf89d1
djllap <kentpercussion@gmail.com>
2016-10-31 13:29:56 -0600

    Added Firebase database

---
+++ src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDotIBG6GEmxwWT9thcxVgwnzZIDPkkHic",
+  authDomain: "catch-of-the-day-kentd.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-kentd.firebaseio.com",
+});
+
+
+export default base;
